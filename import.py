78679192ede24acd64663dc0c8cf2bec8fb9782d
Jia xuan <qwe90307@gmail.com>
2023-11-19 23:08:03 +0800

    add_count_commit

--- import.py
+++ import.py
@@ -1,9 +1,11 @@
 import random
 
 r = random.randint(1,100)
-
+count = 0
 i = 9999
+
 while True:
+	count += 1 # count = count + 1
 	number = input('請輸入1~100的數字: ')
 	number = int(number)
 	if number > r:
@@ -12,4 +14,6 @@ while True:
 		print('再大一些')
 	else:
 		print('答對了!')
+		print('恭喜你猜對了，這是你的第', count, '次')
 		break
+	print('這是你猜的第', count, '次')
